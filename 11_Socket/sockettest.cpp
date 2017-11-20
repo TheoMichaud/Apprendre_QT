a680fdcd5396a7501005d4a51e30be0a8da78dc7
philippeSimier <philaure@wanadoo.fr>
2017-11-20 08:04:52 +0100

    update

--- 11_Socket/sockettest.cpp
+++ 11_Socket/sockettest.cpp
@@ -8,13 +8,10 @@ socketTest::socketTest(QObject *parent) : QObject(parent)
 void socketTest::Connect()
 {
     socket = new QTcpSocket(this);
-<<<<<<< HEAD:11_Socket/socket_01/sockettest.cpp
-    // poirier.depinfo.touchard.edu/siteweb/public/
-    socket->connectToHost("172.18.58.14",80); // adresse du site web de la section port 80
-=======
+
 
     socket->connectToHost("example.com",80);
->>>>>>> 9ec748f22b0760e23797e2f0c4b340dc51b78f1c:11_Socket/sockettest.cpp
+
 
     if(socket->waitForConnected(3000))
     {
